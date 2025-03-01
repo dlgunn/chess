33cbb82ef9b7ea412f59a24fcaf32a8026b65178
dlgunn <dallongunn@gmail.com>
2025-03-01 14:26:41 -0700

    fixed server naming issue

--- server/src/test/java/server/ServerTest.java
+++ server/src/test/java/server/ServerTest.java
@@ -1,8 +1,7 @@
-package Server;
+package server;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import server.Server;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
